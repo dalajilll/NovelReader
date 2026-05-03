250c8e1fb51fe69a48fdacffe1319f6a6ced4f5d
dalajilll <3965799843@qq.com>
2026-05-03 17:06:11 +0800

    Update LocalBookImportActivity.java

--- app/src/main/java/com/novelreader/LocalBookImportActivity.java
+++ app/src/main/java/com/novelreader/LocalBookImportActivity.java
@@ -1,9 +1,11 @@
 package com.novelreader;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.documentfile.provider.DocumentFile;
